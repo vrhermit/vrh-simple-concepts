3fb3467b9d6bbbbb38cdbbaf21b93a9f35f2162b
Joseph Simpson <joe@radicalappdev.com>
2022-04-30 12:11:03 -0400

    Naming things

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Home from "../components/Home.vue";
 import Level1 from "../components/Level1.vue";
 import Level2 from "../components/Level2.vue";
 import Level3 from "../components/Level3.vue";
+import Level4 from "../components/Level4.vue";
 
 // 2. Define some routes
 // Each route should map to a component.
@@ -13,15 +14,17 @@ import Level3 from "../components/Level3.vue";
 const routes = [
   {
     path: "/",
+    name: "home",
     component: Home,
     meta: {
       title: "About",
       subtitle: "This is the home page"
     }
   },
-  { path: "/level1", component: Level1, meta: { title: "Level 1" } },
-  { path: "/level2", component: Level2, meta: { title: "Level 2" } },
-  { path: "/level3", component: Level3, meta: { title: "Level 3" } }
+  { path: "/level1", component: Level1, meta: { title: "Level 1 - Box" } },
+  { path: "/level2", component: Level2, meta: { title: "Level 2 - Sphere" } },
+  { path: "/level3", component: Level3, meta: { title: "Level 3 - Tube" } },
+  { path: "/level4", component: Level4, meta: { title: "Level 4 - Intro" } }
 ];
 
 const router = createRouter({
